c2c1dff7fc9858e93c3cb93035792db40bed15b9
Agustin Suarez <suareza@fullsail.edu>
2012-01-16 19:02:02 -0800

    Re arranged sentence to Boolean Function.

--- suarez_agustin_assignment3.js
+++ suarez_agustin_assignment3.js
@@ -12,7 +12,7 @@
 // Boolean Function
 
 var whatIf = function (win, loose){
-        console.log("What would I do with all the money if I win the tournament?");
+        console.log("What would I do with all the money if I win the tournament \(are my thoughts as I begin to come really close to reaching a set in the finals\)?");
         var result;
         if (win !== true){ result = "I would pay all my debit and buy my parents a house!"; }
         else { result = "I will go home $10.00 dollars less, then what I had to begin with."; }
