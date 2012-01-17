cf167546c4fd40465995a3a3d7d936a5d24f8e28
Agustin Suarez <suareza@fullsail.edu>
2012-01-16 18:59:41 -0800

    Added a Boolean function.

--- suarez_agustin_assignment3.js
+++ suarez_agustin_assignment3.js
@@ -1,1 +1,22 @@
-alert("JavaScript works!");
+/*
+ Agustin Suarez
+ January 16, 2012
+ Project 3
+*/
+
+
+
+
+
+
+// Boolean Function
+
+var whatIf = function (win, loose){
+        console.log("What would I do with all the money if I win the tournament?");
+        var result;
+        if (win !== true){ result = "I would pay all my debit and buy my parents a house!"; }
+        else { result = "I will go home $10.00 dollars less, then what I had to begin with."; }
+        return result;
+};
+var resultBoolean = whatIf(true, false);
+console.log(resultBoolean); // return Boolean
